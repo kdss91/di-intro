f92c8eae3af8b97c03794338591e50241f050bf8
Karandeep Singh <terminatorkaran@gmail.com>
2018-07-04 17:59:27 -0400

    added spring dependency-injection

--- src/main/java/com/springframework/DiIntroApplication.java
+++ src/main/java/com/springframework/DiIntroApplication.java
@@ -1,12 +1,21 @@
 package com.springframework;
 
+import com.springframework.controllers.GetterInjectedController;
+import com.springframework.controllers.PropertyInjectedConroller;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class DiIntroApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DiIntroApplication.class, args);
+        ApplicationContext ctx = SpringApplication.run(DiIntroApplication.class, args);
+
+
+        System.out.println(ctx.getBean(PropertyInjectedConroller.class).sayHello());
+        System.out.println(ctx.getBean(GetterInjectedController.class).sayHello());
+        System.out.println(ctx.getBean(GetterInjectedController.class).sayHello());
+
     }
 }
--- src/main/java/com/springframework/controllers/PropertyInjectedConroller.java
+++ src/main/java/com/springframework/controllers/PropertyInjectedConroller.java
@@ -2,11 +2,15 @@ package com.springframework.controllers;
 
 
 import com.springframework.services.GreetingServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 
+@Controller
 public class PropertyInjectedConroller {
+    @Autowired
     public GreetingServiceImpl greetingService;
 
-    String sayHello(){
+    public String sayHello(){
         return greetingService.sayGreeting();
     }
 
